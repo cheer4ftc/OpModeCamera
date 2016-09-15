1a17f1df5c5d42344cb54b9bec87d06b70fafd03
CHEER4FTC <cheer4ftcblog@gmail.com>
2016-09-15 09:13:16 -0400

    Teardown camera, rotate images

--- sample_camera_opmodes/LinearDetectColor.java
+++ sample_camera_opmodes/LinearDetectColor.java
@@ -5,7 +5,8 @@ import android.graphics.Bitmap;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-import org.firstinspires.ftc.robotcontroller.internal.LinearOpModeCamera;
+import for_camera_opmodes.OpModeCamera;
+import for_camera_opmodes.LinearOpModeCamera;
 
 /**
  * TeleOp Mode
@@ -13,48 +14,47 @@ import org.firstinspires.ftc.robotcontroller.internal.LinearOpModeCamera;
  * Enables control of the robot via the gamepad
  */
 
-@TeleOp(name="LinearDetectColor", group="ZZOpModeCameraPackage")
+@TeleOp(name = "LinearDetectColor", group = "ZZOpModeCameraPackage")
 //@Disabled
 public class LinearDetectColor extends LinearOpModeCamera {
 
-    DcMotor motorRight;
-    DcMotor motorLeft;
+  DcMotor motorRight;
+  DcMotor motorLeft;
 
-    int ds2 = 2;  // additional downsampling of the image
-    // set to 1 to disable further downsampling
+  int ds2 = 2;  // additional downsampling of the image
+  // set to 1 to disable further downsampling
 
-    @Override
-    public void runOpMode() throws InterruptedException {
+  @Override
+  public void runOpMode() throws InterruptedException {
 
-        String colorString = "NONE";
+    String colorString = "NONE";
 
-        // linear OpMode, so could do stuff like this too.
+    // linear OpMode, so could do stuff like this too.
         /*
         motorLeft = hardwareMap.dcMotor.get("motor_1");
         motorRight = hardwareMap.dcMotor.get("motor_2");
         motorLeft.setDirection(DcMotor.Direction.REVERSE);
         */
 
-        if (isCameraAvailable()) {
+    if (isCameraAvailable()) {
 
-            setCameraDownsampling(8);
-            // parameter determines how downsampled you want your images
-            // 8, 4, 2, or 1.
-            // higher number is more downsampled, so less resolution but faster
-            // 1 is original resolution, which is detailed but slow
-            // must be called before super.init sets up the camera
+      setCameraDownsampling(8);
+      // parameter determines how downsampled you want your images
+      // 8, 4, 2, or 1.
+      // higher number is more downsampled, so less resolution but faster
+      // 1 is original resolution, which is detailed but slow
+      // must be called before super.init sets up the camera
 
-            startCamera();  // can take a while.
-                            // best started before waitForStart
-                            // or in a separate thread.
+      telemetry.addLine("Wait for camera to finish initializing!");
+      telemetry.update();
+      startCamera();  // can take a while.
+      // best started before waitForStart
+      telemetry.addLine("Camera ready!");
+      telemetry.update();
 
-            waitForStart();
+      waitForStart();
 
-            stopCameraInSecs(30);   // set independent thread to kill the camera
-                                    // when the mode is done
-                                    // use 30 for auto, 120 for teleop
-
-            // LinearOpMode, so could do stuff like this too.
+      // LinearOpMode, so could do stuff like this too.
         /*
         motorLeft.setPower(1);  // drive forward
         motorRight.setPower(1);
@@ -63,46 +63,49 @@ public class LinearDetectColor extends LinearOpModeCamera {
         motorRight.setPower(0);
         sleep(1000);            // wait a second.
         */
+      try { // try is needed so catch the interrupt when the opmode is ended to stop the camera
+        while (opModeIsActive()) {
+          if (imageReady()) { // only do this if an image has been returned from the camera
+            int redValue = 0;
+            int blueValue = 0;
+            int greenValue = 0;
 
-            while (opModeIsActive()) {
-                if (imageReady()) { // only do this if an image has been returned from the camera
-                    int redValue = 0;
-                    int blueValue = 0;
-                    int greenValue = 0;
+            // get image, rotated so (0,0) is in the bottom left of the preview window
+            Bitmap rgbImage;
+            rgbImage = convertYuvImageToRgb(yuvImage, width, height, ds2);
 
-                    Bitmap rgbImage;
-                    rgbImage = convertYuvImageToRgb(yuvImage, width, height, ds2);
-                    for (int x = 0; x < width / ds2; x++) {
-                        for (int y = 0; y < height / ds2; y++) {
-                            int pixel = rgbImage.getPixel(x, y);
-                            redValue += red(pixel);
-                            blueValue += blue(pixel);
-                            greenValue += green(pixel);
-                        }
-                    }
-                    int color = highestColor(redValue, greenValue, blueValue);
+            for (int x = 0; x < rgbImage.getWidth(); x++) {
+              for (int y = 0; y < rgbImage.getHeight(); y++) {
+                int pixel = rgbImage.getPixel(x, y);
+                redValue += red(pixel);
+                blueValue += blue(pixel);
+                greenValue += green(pixel);
+              }
+            }
+            int color = highestColor(redValue, greenValue, blueValue);
 
-                    switch (color) {
-                        case 0:
-                            colorString = "RED";
-                            break;
-                        case 1:
-                            colorString = "GREEN";
-                            break;
-                        case 2:
-                            colorString = "BLUE";
-                    }
-
-                } else {
-                    colorString = "NONE";
-                }
-
-                telemetry.addData("Color:", "Color detected is: " + colorString);
-                telemetry.update();
-                sleep(10);
+            switch (color) {
+              case 0:
+                colorString = "RED";
+                break;
+              case 1:
+                colorString = "GREEN";
+                break;
+              case 2:
+                colorString = "BLUE";
             }
 
-            stopCamera();
+          } else {
+            colorString = "NONE";
+          }
+
+          telemetry.addData("Color:", "Color detected is: " + colorString);
+          telemetry.update();
+          sleep(10);
         }
+      } catch (Exception e) {
+        stopCamera();
+      }
     }
+  }
 }
